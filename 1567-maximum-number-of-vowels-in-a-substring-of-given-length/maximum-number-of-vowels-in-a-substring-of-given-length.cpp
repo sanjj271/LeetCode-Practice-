f2d88549745ef5e4d1d57c7377643c027309f429
Sanjana gogte <sanj27272@gmail.com>
2024-09-17 20:32:39 +0530

    Time: 160 ms (5.02%) | Memory: 11.5 MB (83.71%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,43 +1,29 @@
 class Solution {
 public:
     int maxVowels(string s, int k) {
-    int i =0;
-    int j =0;
-    int count =0;
-    int max_count = INT_MIN;
-    map<char,int> mp;
-    while(j<s.size()){
-        if((j-i)<k-1){
-            mp[s[j]]++;
-            if(isVowel(s[j])){
-                count++;
+    int left = 0;
+    int vcount =0;
+    int maxcount =0;
+    for(int right =0;right<s.size();right++){
+        if((right-left)+1 < k){
+            if(isVowel(s[right])){
+                vcount++;
             }
-
+        }
+        else if((right-left) + 1 == k){
+            if(isVowel(s[right])){
+                vcount++;
+            }
+            cout<<"VCOUNT"<<vcount<<endl;
+            maxcount = max(vcount,maxcount);
+            if(isVowel(s[left])){
+                vcount--;
+            }
+            left ++;
 
         }
-        if((j-i)==k-1){
-            mp[s[j]]++;
-            if(isVowel(s[j])){
-                cout<<s[j]<<endl;
-                count++;
-            }
-            max_count = max(count,max_count);
-            cout<<count<<endl;
-            if(isVowel(s[i])){
-                cout<<s[i]<<endl;
-                count--;
-            }
-            mp[s[i]]--;
-            if(mp[s[i]]==0){
-                mp.erase(s[i]);
-            }
-            i++;
-
-        }
-        j++;
     }
-    return max_count;
-    
+    return maxcount;
         
     }
     bool isVowel(char s){
@@ -45,7 +31,5 @@ public:
         return true;
     }
     return false;
-
-
-    }
+}
 };
